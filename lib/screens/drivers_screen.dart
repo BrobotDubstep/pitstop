304b1f5f1a470ac16ee2393e01e1ad3fbeaa3aad
Tim Olbrich <tim96@live.de>
2021-08-03 20:32:16 +0200

    Dropdown for year of the season

--- lib/screens/drivers_screen.dart
+++ lib/screens/drivers_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class DriversScreen extends StatelessWidget {
+class DriversScreen extends StatefulWidget {
   const DriversScreen({Key? key}) : super(key: key);
+
+  @override
+  _DriversScreenState createState() => _DriversScreenState();
+}
+
+class _DriversScreenState extends State<DriversScreen> {
+  String dropdownValue = "2021";
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +19,32 @@ class DriversScreen extends StatelessWidget {
           children: [
             Padding(
               padding: const EdgeInsets.only(left: 35.0, top: 30.0),
-              child: Text(
-                'Drivers of 2021',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
-              ),
+              child: Row(children: [
+                Text(
+                  'Drivers of ',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+                ),
+                DropdownButtonHideUnderline(
+                  child: DropdownButton(
+                      value: dropdownValue,
+                      style: TextStyle(
+                          color: Colors.deepPurple,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          dropdownValue = newValue!;
+                        });
+                      },
+                      items: <String>['2021', '2020', '2019', '2018']
+                          .map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList()),
+                )
+              ]),
             ),
           ],
         ),
