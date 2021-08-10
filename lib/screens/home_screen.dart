b75aacbf416c40f92002a9d114f924cc6d2037a9
Tim Olbrich <tim96@live.de>
2021-08-10 15:30:54 +0200

    added fl_chart dependency

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pitstop/providers/season_provider.dart';
